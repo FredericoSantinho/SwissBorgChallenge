0f21340c4dab70065d67c6aa606666a1277429cd
Neurophobic Animal <fredericosantinho@gmail.com>
2024-07-13 20:48:35 +0100

    Add symbol pairs list.

--- composeApp/src/commonMain/kotlin/neuro/swissborg/viewmodel/AppViewModel.kt
+++ composeApp/src/commonMain/kotlin/neuro/swissborg/viewmodel/AppViewModel.kt
@@ -15,8 +15,8 @@ class AppViewModel(
 ) : ViewModel() {
 	fun onClick() {
 		viewModelScope.launch {
-			val symbolsList = listOf("BTCUSD", "ETHUSD")
-			val tickers = tickersRepository.getTickers(symbolsList)
+			val symbolPairsList = getSymbolPairsList()
+			val tickers = tickersRepository.getTickers(symbolPairsList)
 
 			println("blaz $tickers")
 
@@ -25,9 +25,18 @@ class AppViewModel(
 
 			println("blaz3 ${Color.Green.value} ${Color.Red.value}")
 
-			val execute = getCoinsDetailsUseCase.execute(symbolsList)
+			val execute = getCoinsDetailsUseCase.execute(symbolPairsList)
 
 			println("blaz4 $execute")
 		}
+	}
+
+	private fun getSymbolPairsList(): List<String> {
+		val symbolsQuery =
+			"?symbols=tBTCUSD,tETHUSD,tCHSB:USD,tLTCUSD,tXRPUSD,tDSHUSD,tRRTUSD,tEOSUSD,tSANUSD,tDATUSD,tSNTUSD,tDOGE:USD,tLUNA:USD,tMATIC:USD,tNEXO:USD, tOCEAN:USD, tBEST:USD, tAAVE:USD, tPLUUSD, tFILUSD"
+				.replace("?symbols=", "")
+		val split = symbolsQuery.split(",").map { it.substring(1) }
+
+		return split
 	}
 }
