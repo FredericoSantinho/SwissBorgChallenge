eb1a33e2f0bbe0713f661e0c3d4fbc0a14854500
Neurophobic Animal <fredericosantinho@gmail.com>
2024-07-13 22:06:03 +0100

    Cleanup.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -31,10 +31,6 @@ kotlin {
 	}
 
 	sourceSets {
-
-		jvmMain.dependencies {
-			implementation(libs.ktor.client.okhttp)
-		}
 		androidMain.dependencies {
 			implementation(compose.preview)
 			implementation(libs.androidx.activity.compose)
