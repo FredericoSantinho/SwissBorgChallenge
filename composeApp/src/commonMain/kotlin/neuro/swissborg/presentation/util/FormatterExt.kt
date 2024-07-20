a93988e739cb0444298181edf522480b62c3fc96
Neurophobic Animal <fredericosantinho@gmail.com>
2024-07-21 00:52:51 +0100

    FormatterExt now supports fully rounding.

--- composeApp/src/commonMain/kotlin/neuro/swissborg/presentation/util/FormatterExt.kt
+++ composeApp/src/commonMain/kotlin/neuro/swissborg/presentation/util/FormatterExt.kt
@@ -1,31 +1,70 @@
 package neuro.swissborg.presentation.util
 
-import kotlin.math.min
+import kotlin.math.pow
 
-fun Double.format(maxDecimals: Int = 2): String {
-	val result = this.toString()
-	val lastIndex = result.length - 1
-	var pos = lastIndex
-	while (pos >= 0 && result[pos] != '.') {
-		pos--
-	}
-	return if (maxDecimals < 1 && pos >= 0) {
-		result.substring(0, min(pos, result.length))
-	} else if (pos >= 0) {
-		val substring = result.substring(0, min(pos + 1 + maxDecimals + 1, result.length))
-		val substringLastDigit = substring[substring.length - 1].digitToInt()
-		if (substringLastDigit in 5..8) {
-			val notRoundedResult = result.substring(0, min(pos + 1 + maxDecimals, result.length))
-			notRoundedResult.substring(
-				0,
-				notRoundedResult.length - 1
-			) + (notRoundedResult[notRoundedResult.length - 1].digitToInt() + 1)
+fun Double.format(decimals: Int = 2): String {
+	val stringValue = this.toString()
+	// We need this 0.0000000000001 bias to overcome double representation limitations
+	val initialLongValue = ((10.0.pow(decimals) + 0.0000000000001) * this).toLong()
+	var longValue = initialLongValue
+
+	if (stringValue.indexOf('.') + decimals + 1 < stringValue.length
+		&& stringValue[stringValue.indexOf('.') + decimals + 1].digitToInt() >= 5
+	) {
+		if (longValue > 0) {
+			longValue++
 		} else {
-			result.substring(0, min(pos + 1 + maxDecimals, result.length))
+			longValue--
 		}
-	} else {
-		return result
 	}
+
+	var lostAZero = false
+	if (longValue.toString().length > initialLongValue.toString().length) {
+		lostAZero = true
+	}
+
+	val leftZeroes = countLeftZeroes(stringValue, decimals)
+
+	return buildString(longValue, decimals, leftZeroes, lostAZero)
+}
+
+private fun countLeftZeroes(stringValue: String, decimals: Int): Int {
+	var n = 0
+	for (i in 0..if (stringValue.startsWith("-")) decimals + 1 else decimals) {
+		if (stringValue[i] == '0' || stringValue[i] == '.' || stringValue[i] == '-') {
+			if (stringValue[i] == '0') {
+				n++
+			}
+		} else {
+			break
+		}
+	}
+	return n
+}
+
+private fun buildString(
+	longValue: Long,
+	decimals: Int,
+	leftZeroes: Int,
+	lostAZero: Boolean,
+): String {
+	val tmpString = longValue.toString()
+	val newLeftZeroes = if (lostAZero) leftZeroes - 1 else leftZeroes
+	val string = addLeftZeroes(tmpString, newLeftZeroes)
+
+	return if (string.startsWith("-")) {
+		"-" + string.substring(1, string.length - decimals) +
+				"." + string.substring(string.length - decimals, string.length)
+	} else {
+		string.substring(0, string.length - decimals) +
+				"." + string.substring(string.length - decimals, string.length)
+	}
+}
+
+private fun addLeftZeroes(string: String, leftZeroes: Int): String {
+	return if (string.startsWith("-")) "-" + (1..leftZeroes).map { "0" }
+		.joinToString(separator = "") { it } + string.substring(1) else (1..leftZeroes).map { "0" }
+		.joinToString(separator = "") { it } + string
 }
 
 fun Double.formatCurrency(
--- composeApp/src/commonTest/kotlin/neuro/swissborg/presentation/util/FormatterExtTest.kt
+++ composeApp/src/commonTest/kotlin/neuro/swissborg/presentation/util/FormatterExtTest.kt
@@ -5,40 +5,74 @@ import kotlin.test.assertEquals
 
 class FormatterExtTest {
 	@Test
-	fun testFormat() {
-		assertEquals("100.0", 100.0.format(2))
-		assertEquals("100.0", 100.00.format(2))
+	fun testFormatTwoDecimals() {
+		assertEquals("100.00", 100.0.format(2))
+		assertEquals("100.00", 100.00.format(2))
 		assertEquals("100.01", 100.01.format(2))
 		assertEquals("100.02", 100.015.format(2))
-		assertEquals("0.1", 0.1.format(2))
+		assertEquals("0.10", 0.1.format(2))
 		assertEquals("0.01", 0.01.format(2))
 		assertEquals("0.02", 0.015.format(2))
-		// Rounding this type of input represents an effort that is not worth it.
-		assertEquals("0.99", 0.999.format(2))
-		assertEquals("-0.1", (-0.1).format(2))
+		assertEquals("1.00", 0.999.format(2))
+		assertEquals("-0.10", (-0.1).format(2))
 		assertEquals("-0.01", (-0.01).format(2))
 		assertEquals("-0.02", (-0.015).format(2))
-		assertEquals("-1.1", (-1.1).format(2))
+		assertEquals("-1.10", (-1.1).format(2))
 		assertEquals("-1.01", (-1.01).format(2))
 		assertEquals("-1.02", (-1.015).format(2))
 	}
 
 	@Test
+	fun testFormatThreeDecimals() {
+		assertEquals("100.000", 100.0.format(3))
+		assertEquals("100.000", 100.00.format(3))
+		assertEquals("100.001", 100.001.format(3))
+		assertEquals("100.002", 100.0015.format(3))
+		assertEquals("0.010", 0.01.format(3))
+		assertEquals("0.001", 0.001.format(3))
+		assertEquals("0.002", 0.0015.format(3))
+		assertEquals("1.000", 0.9999.format(3))
+		assertEquals("-0.100", (-0.1).format(3))
+		assertEquals("-0.001", (-0.001).format(3))
+		assertEquals("-0.002", (-0.0015).format(3))
+		assertEquals("-1.100", (-1.1).format(3))
+		assertEquals("-1.001", (-1.001).format(3))
+		assertEquals("-1.002", (-1.0015).format(3))
+	}
+
+	@Test
 	fun testFormatCurrency() {
-		assertEquals("$ 100.0", 100.0.formatCurrency(2))
-		assertEquals("$ 100.0", 100.00.formatCurrency(2))
+		assertEquals("$ 100.00", 100.0.formatCurrency(2))
+		assertEquals("$ 100.00", 100.00.formatCurrency(2))
 		assertEquals("$ 100.01", 100.01.formatCurrency(2))
 		assertEquals("$ 100.02", 100.015.formatCurrency(2))
-		assertEquals("$ 0.1", 0.1.formatCurrency(2))
+		assertEquals("$ 0.10", 0.1.formatCurrency(2))
 		assertEquals("$ 0.01", 0.01.formatCurrency(2))
 		assertEquals("$ 0.02", 0.015.formatCurrency(2))
-		// Rounding this type of input represents an effort that is not worth it.
-		assertEquals("$ 0.99", 0.999.formatCurrency(2))
-		assertEquals("$ -0.1", (-0.1).formatCurrency(2))
+		assertEquals("$ 1.00", 0.999.formatCurrency(2))
+		assertEquals("$ -0.10", (-0.1).formatCurrency(2))
 		assertEquals("$ -0.01", (-0.01).formatCurrency(2))
 		assertEquals("$ -0.02", (-0.015).formatCurrency(2))
-		assertEquals("$ -1.1", (-1.1).formatCurrency(2))
+		assertEquals("$ -1.10", (-1.1).formatCurrency(2))
 		assertEquals("$ -1.01", (-1.01).formatCurrency(2))
 		assertEquals("$ -1.02", (-1.015).formatCurrency(2))
+	}
+
+	@Test
+	fun testFormatCurrencyThreeDecimals() {
+		assertEquals("$ 100.000", 100.0.formatCurrency(3))
+		assertEquals("$ 100.000", 100.00.formatCurrency(3))
+		assertEquals("$ 100.001", 100.001.formatCurrency(3))
+		assertEquals("$ 100.002", 100.0015.formatCurrency(3))
+		assertEquals("$ 0.010", 0.01.formatCurrency(3))
+		assertEquals("$ 0.001", 0.001.formatCurrency(3))
+		assertEquals("$ 0.002", 0.0015.formatCurrency(3))
+		assertEquals("$ 1.000", 0.9999.formatCurrency(3))
+		assertEquals("$ -0.100", (-0.1).formatCurrency(3))
+		assertEquals("$ -0.001", (-0.001).formatCurrency(3))
+		assertEquals("$ -0.002", (-0.0015).formatCurrency(3))
+		assertEquals("$ -1.100", (-1.1).formatCurrency(3))
+		assertEquals("$ -1.001", (-1.001).formatCurrency(3))
+		assertEquals("$ -1.002", (-1.0015).formatCurrency(3))
 	}
 }
